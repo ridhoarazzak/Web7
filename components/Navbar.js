70636820d197fce1838fca3b4cfa348621501e1a
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-07-05 01:27:11 +0700

    Update Navbar.js
    Innit update

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,17 +3,18 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow-md w-full">
+    // Beri sedikit warna pada Navbar
+    <nav className="bg-white/80 shadow-md w-full backdrop-blur-sm sticky top-0 z-50">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <Link href="/" className="font-bold text-2xl text-blue-600">
+        <Link href="/" className="font-extrabold text-2xl text-brand-green">
           Penginapan Asri
         </Link>
-        <div className="space-x-4">
-          <Link href="/" className="text-gray-600 hover:text-blue-500">Beranda</Link>
-          <Link href="/#kamar" className="text-gray-600 hover:text-blue-500">Kamar</Link> {/* Nanti bisa diubah */}
-          <Link href="/kontak" className="text-gray-600 hover:text-blue-500">Kontak</Link>
+        <div className="space-x-6">
+          <Link href="/" className="text-brand-dark hover:text-brand-green font-semibold">Beranda</Link>
+          <Link href="/#kamar" className="text-brand-dark hover:text-brand-green font-semibold">Kamar</Link>
+          <Link href="/kontak" className="text-brand-dark hover:text-brand-green font-semibold">Kontak</Link>
         </div>
       </div>
     </nav>
   );
-}
+    }
