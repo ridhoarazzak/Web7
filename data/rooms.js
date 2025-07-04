affc25839478e860b04875b633ad615423d9040d
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-07-05 00:12:40 +0700

    Create rooms.js
    Innit

---
+++ data/rooms.js
@@ -0,0 +1,24 @@
+// data/rooms.js
+export const rooms = [
+  {
+    id: 'standard',
+    name: 'Kamar Standard',
+    description: 'Kamar yang nyaman dan ekonomis, cocok untuk solo traveler atau pasangan. Dilengkapi dengan AC, TV, dan kamar mandi pribadi.',
+    price: 350000,
+    image: '/images/room-standard.jpg',
+  },
+  {
+    id: 'deluxe',
+    name: 'Kamar Deluxe',
+    description: 'Lebih luas dari kamar standard dengan pemandangan ke arah taman. Fasilitas tambahan termasuk mini-fridge dan pembuat kopi.',
+    price: 550000,
+    image: '/images/room-deluxe.jpg',
+  },
+  {
+    id: 'suite',
+    name: 'Kamar Suite',
+    description: 'Puncak kemewahan dan kenyamanan. Memiliki ruang tamu terpisah, bathtub, dan akses eksklusif ke lounge.',
+    price: 900000,
+    image: '/images/room-suite.jpg',
+  },
+];
